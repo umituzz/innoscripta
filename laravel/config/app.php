61f5dacbf552014f1a7dd31b64ff7dde7d01b8d8
Ümit UZ <umituz9999@gmail.com>
2023-09-20 22:58:11 +0300

    .

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
         Laravel\Scout\ScoutServiceProvider::class,
     ])->toArray(),
