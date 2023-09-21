65d5113d569ad25970b8671b8b70f73f39b2713f
Ümit UZ <umituz9999@gmail.com>
2023-09-21 15:53:31 +0300

    .

--- react/web-vitals.js
+++ react/web-vitals.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { getCLS, getFID, getLCP, getTTFB } from 'web-vitals';
 
 function reportToAnalytics({ name, delta, id }) {
-    console.log(name, delta, id);
+    // console.log(name, delta, id);
 }
 
 function WebVitals() {
