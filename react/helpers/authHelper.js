86abf8d7442a8f0d04dcd9aad5a8e2da38fb55ce
Ümit UZ <umituz9999@gmail.com>
2023-09-21 16:37:51 +0300

    .

--- react/helpers/authHelper.js
+++ react/helpers/authHelper.js
@@ -1,5 +1,19 @@
-import {useSelector} from "react-redux";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 
-export const authCheck = () => {
-    return useSelector((state) => state.authReducer.isAuthenticated);
-}
+export const authCheck = (redirectToLogin = true) => {
+    const router = useRouter();
+    const isLoggedIn = useSelector((state) => state.authReducer.token);
+
+    if (!isLoggedIn && redirectToLogin) {
+        if (typeof window !== "undefined") {
+            router.push("/login");
+        }
+    } else if (!isLoggedIn && redirectToLogin) {
+        if (typeof window !== "undefined") {
+            router.push("/");
+        }
+    }
+
+    return isLoggedIn;
+};
--- react/pages/index.tsx
+++ react/pages/index.tsx
@@ -1,14 +1,14 @@
-import {Row, Container} from "react-bootstrap";
+import { Row, Container } from "react-bootstrap";
 import Link from "next/link";
-import {authCheck} from "../helpers/authHelper";
 import HeadComponent from "../components/HeadComponent";
+import { authCheck } from "../helpers/authHelper";
 
 export default function Home() {
     const isLoggedIn = authCheck();
 
     return (
         <Container className="mt-2 minHeight">
-            <HeadComponent title={`Homepage`}/>
+            <HeadComponent title={`Homepage`} />
             <Row>
                 {isLoggedIn ? (
                     <Link href="/articles">Articles</Link>
