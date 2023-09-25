f2852bcd3b03223cfe4393a401e1f0570efefef1
Ümit UZ <umituz9999@gmail.com>
2023-09-25 15:59:04 +0300

    .

--- frontend/contexts/AppContext.js
+++ frontend/contexts/AppContext.js
@@ -2,25 +2,25 @@ import React from 'react';
 import {LoginProvider} from './LoginContext';
 import {RegisterProvider} from './RegisterContext';
 import {ArticleProvider} from "./ArticleContext";
-import {PreferenceProvider} from "./PreferenceContext";
-import {authToken} from "../helpers/authHelper";
+//import {PreferenceProvider} from "./PreferenceContext";
+// import {authToken} from "../helpers/authHelper";
 
 const AppProvider = ({children}) => {
-    const token = authToken();
+    // const token = authToken();
 
-    if (token) {
-        return (
-            <LoginProvider>
-                 <RegisterProvider>
-                     <ArticleProvider>
-                         <PreferenceProvider>
-                             {children}
-                         </PreferenceProvider>
-                     </ArticleProvider>
-                 </RegisterProvider>
-             </LoginProvider>
-         )
-     } else {
+    // if (token) {
+    //     return (
+    //         <LoginProvider>
+    //              <RegisterProvider>
+    //                  <ArticleProvider>
+    //                      <PreferenceProvider>
+    //                          {children}
+    //                      </PreferenceProvider>
+    //                  </ArticleProvider>
+    //              </RegisterProvider>
+    //          </LoginProvider>
+    //      )
+    //  } else {
         return (
             <LoginProvider>
                 <RegisterProvider>
@@ -30,7 +30,7 @@ const AppProvider = ({children}) => {
                 </RegisterProvider>
             </LoginProvider>
         )
-     }
+     // }
 };
 
 export default AppProvider;
