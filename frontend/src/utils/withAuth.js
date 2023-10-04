729431bccc5fd1e16aa79085ca5b740fc22665be
Ümit UZ <umituz9999@gmail.com>
2023-10-04 14:57:01 +0300

    .

--- frontend/src/utils/withAuth.js
+++ frontend/src/utils/withAuth.js
@@ -1,21 +1,28 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import {useSelector} from "react-redux";
+import { useSelector } from 'react-redux';
 
 function withAuth(WrappedComponent) {
-    return (props) => {
+    const Wrapper = (props) => {
         const router = useRouter();
         const token = useSelector((state) => state.authReducer.token);
 
         useEffect(() => {
-
             if (!token) {
                 router.push('/login');
             }
-        }, []);
+        }, [router, token]);
 
         return <WrappedComponent {...props} />;
     };
+
+    if (WrappedComponent.displayName) {
+        Wrapper.displayName = `withAuth(${WrappedComponent.displayName})`;
+    } else if (WrappedComponent.name) {
+        Wrapper.displayName = `withAuth(${WrappedComponent.name})`;
+    }
+
+    return Wrapper;
 }
 
 export default withAuth;
