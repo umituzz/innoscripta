3a2d76f5463993e9da08377a358c2b03495f0f7e
Ümit UZ <umituz9999@gmail.com>
2023-10-07 02:06:41 +0300

    .

--- microservices/python/app.py
+++ microservices/python/app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 
+mongo_uri = "mongodb://localhost:27017"
+db_name = "mongodb"
+collection_name = "logs"
+client = MongoClient(mongo_uri)
+db = client[db_name]
+collection = db[collection_name]
+
 @app.route('/')
 def hello_world():
+    log_entry = {
+        "timestamp": datetime.now(),
+        "message": "A new request for / endpoint in Python Flask project"
+    }
+
+    collection.insert_one(log_entry)
+
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=6000)
+    app.run(debug=True, host='0.0.0.0', port=8085)
